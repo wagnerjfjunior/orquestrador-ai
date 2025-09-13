c52dd0e0cc5612d704aabc5244503b62df96d93e
Wagner Fernandes <wagnerjfjunior@gmail.com>
2025-09-13 00:32:52 -0300

    test: add basic tests; feat: observability & metrics modules

--- app/metrics.py
+++ app/metrics.py
@@ -2,7 +2,4 @@ from prometheus_fastapi_instrumentator import Instrumentator
 from fastapi import FastAPI
 
 def setup_metrics(app: FastAPI) -> None:
-    """
-    Monta /metrics e coleta métricas de latência, contadores por rota/status, etc.
-    """
     Instrumentator().instrument(app).expose(app, endpoint="/metrics")
